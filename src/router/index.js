ac90bb3a70d11c60b5dff64a391d4fe1892bd5aa
Juan-GR <juanos0808@gmail.com>
2024-06-19 20:39:12 +0200

    feat(Admin) Create routes and components for admin panel

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,28 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/LoginView.vue')
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: () => import('../views/admin/AdminLayout.vue'),
+      children: [
+        {
+          path: '/admin/propiedades',
+          name: 'admin-properties',
+          component: () => import('../views/admin/AdminView.vue')
+        },
+        {
+          path: '/admin/nueva',
+          name: 'admin-new-property',
+          component: () => import('../views/admin/NewPropertyView.vue')
+        },
+        {
+          path: '/admin/editar/:id',
+          name: 'admin-edit-property',
+          component: () => import('../views/admin/EditPropertyView.vue')
+        }
+      ]
     }
   ]
 })
